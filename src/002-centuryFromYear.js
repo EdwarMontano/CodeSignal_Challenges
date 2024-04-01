ed5a05f2ff22a7627a78ae95543b3623c00efd83
Edwar Montano MacOsx <edwar.montano@correounivalle.edu.co>
2024-04-01 13:09:58 -0500

    style: format doc

--- src/002-centuryFromYear.js
+++ src/002-centuryFromYear.js
@@ -1,20 +1,20 @@
 /**
  * Returns the century of the given year.
- * 
+ *
  * The first century spans from the year 1 up to and including the year 100,
  * the second century from the year 101 up to and including the year 200, etc.
- * 
+ *
  * @example
  * // returns 20
  * solution(1905);
- * 
+ *
  * @example
  * // returns 17
  * solution(1700);
- * 
+ *
  * @param {number} year - A positive integer designating the year. Must be between 1 and 2005, inclusive.
  * @return {number} - The century of the given year.
- * 
+ *
  * @constraints
  * - Execution time limit: 4 seconds
  * - Memory limit: 1GB
--- src/003-polindrome.js
+++ src/003-polindrome.js
@@ -1,23 +1,23 @@
 /**
  * Checks if a given string is a palindrome.
- * 
+ *
  * A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
- * 
+ *
  * @example
  * // returns true
  * solution("aabaa");
- * 
+ *
  * @example
  * // returns false
  * solution("abac");
- * 
+ *
  * @example
  * // returns true
  * solution("a");
- * 
+ *
  * @param {string} inputString - A non-empty string consisting of lowercase characters. The length of the string is guaranteed to be between 1 and 10^5.
  * @return {boolean} - Returns true if `inputString` is a palindrome, false otherwise.
- * 
+ *
  * @constraints
  * - Execution time limit: 4 seconds
  * - Memory limit: 1GB
